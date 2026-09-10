5a759c85b57155777004950c679567759059be08
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-09-10 14:59:03 -0300

    first commit

---
+++ src/main/java/br/com/guilhermesalvador/uaheroacademyapi/UaHeroAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermesalvador.uaheroacademyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaHeroAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaHeroAcademyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guilhermesalvador/uaheroacademyapi/UaHeroAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermesalvador.uaheroacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaHeroAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
