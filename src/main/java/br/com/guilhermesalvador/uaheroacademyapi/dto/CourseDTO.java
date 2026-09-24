5a2ed757778a30983210ff888efbf027eef66468
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-09-24 14:29:48 -0300

    feat: Create DTO and Mapper files for Course entity

---
+++ src/main/java/br/com/guilhermesalvador/uaheroacademyapi/dto/CourseDTO.java
@@ -0,0 +1,4 @@
+package br.com.guilhermesalvador.uaheroacademyapi.dto;
+
+public class CourseDTO {
+}
---
+++ src/main/java/br/com/guilhermesalvador/uaheroacademyapi/mapper/CourseMapper.java
@@ -0,0 +1,4 @@
+package br.com.guilhermesalvador.uaheroacademyapi.mapper;
+
+public class CourseMapper {
+}
