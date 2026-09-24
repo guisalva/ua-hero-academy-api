b4b69a8f66978a9cc90b4195cb6a9cc0937f8626
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-09-24 17:31:13 -0300

    feat: Create Course DTO and Mapper to achieve more abstraction

--- src/main/java/br/com/guilhermesalvador/uaheroacademyapi/mapper/CourseMapper.java
+++ src/main/java/br/com/guilhermesalvador/uaheroacademyapi/mapper/CourseMapper.java
@@ -1,4 +1,30 @@
 package br.com.guilhermesalvador.uaheroacademyapi.mapper;
 
+import br.com.guilhermesalvador.uaheroacademyapi.dto.CourseDTO;
+import br.com.guilhermesalvador.uaheroacademyapi.model.CourseModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CourseMapper {
+
+    public CourseModel map(CourseDTO courseDTO) {
+        CourseModel courseModel = new CourseModel();
+        courseModel.setId(courseDTO.getId());
+        courseModel.setTitle(courseDTO.getTitle());
+        courseModel.setDescription(courseDTO.getDescription());
+        courseModel.setClasses(courseDTO.getClasses());
+
+        return courseModel;
+    }
+
+    public CourseDTO map(CourseModel courseModel) {
+        CourseDTO courseDTO = new CourseDTO();
+        courseDTO.setId(courseModel.getId());
+        courseDTO.setTitle(courseModel.getTitle());
+        courseDTO.setDescription(courseModel.getDescription());
+        courseDTO.setClasses(courseModel.getClasses());
+
+        return courseDTO;
+    }
+
 }
